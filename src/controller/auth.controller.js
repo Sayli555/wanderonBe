4796943b84f0e9125096a5f7d4638118805daf25
sayali giri <sayligiri555@gmail.com>
2024-06-10 18:42:00 +0530

    error handling done

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,6 +1,7 @@
+const { validationResult } = require("express-validator")
 const User=require("../models/user.model")
 const jwt=require("jsonwebtoken")
-
+const mongoSanitize = require('mongo-sanitize')
 
 const newTocken=(user)=>{
     return jwt.sign({user:user},process.env.JWT_SECRET_KEY)
@@ -9,16 +10,34 @@ const newTocken=(user)=>{
 const register=async(req,res)=>{
     let user;
     try {
-        user= await User.findOne({email:req.body.email}).lean().exec()
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ status: "failed", errors: errors.array() });
+        }
+
+
+        if(!req.body.name || !req.body.email || !req.body.password){
+            return res.status(400).send({message:"please provide require information",flag:false})
+        }
+
+
+        const sanitizedBody = mongoSanitize(req.body);
+        user= await User.findOne({email:sanitizedBody.email}).lean().exec()
         if(user){
             return res.status(400).send({status:"failed",message:"Please login with another email"})
         }
-        user=await User.create(req.body)
+
+
+        user=await User.create(sanitizedBody)
         if(!user){
             return res.status(500).send({status:"failed",message:"Please try again letter"})
         }
+
+
         let token=newTocken(user)
         res.status(200).json({user,token})
+
     } catch (error) {
         return res.status(400).send({status:"failed",message:"Please login with another email"})
     }    
@@ -26,7 +45,20 @@ const register=async(req,res)=>{
 
 const login=async(req,res)=>{
     let user; 
+
+
     try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ status: "failed", errors: errors.array() });
+        }
+
+
+        if(!req.body.email || !req.body.password){
+            return res.status(400).send({message:"please provide require information",flag:false})
+        }
+
+
         user=await User.findOne({email:req.body.email}).exec()
         if(!user) {return res.status(400).send({status:"failed",message:"Please login with another email"})}
        let match=await user.checkPassword(req.body.password)  
@@ -34,10 +66,14 @@ const login=async(req,res)=>{
        user={
         ...user._doc
        }
+
+
        delete user.password
        let token=newTocken(user)
        res.cookie('token', token, { httpOnly: true, secure: true });
        res.status(200).json({token:token,user:user})
+
+
     } catch (error) {
         return res.status(400).send({status:"failed",message:"Please login with another email and password"})
     }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,7 +5,7 @@ const userSchema=new mongoose.Schema({
     name:{type:String,require:true},
     email:{type:String,require:true},
     password:{type:String,require:true},
-    roles:[{type:String,require:true}]
+    roles:[{type:String,require:false}]
 },{
     versionKey:false,
     timestamps:true
